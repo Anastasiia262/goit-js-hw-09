1c8221a136030bcbc3539b560ec5e839addfbf15
Anastasiia262 <gribskay2@gmail.com>
2023-05-08 21:09:46 +0300

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,34 +1,43 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+
 
 function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
+    
     setTimeout(() => {
+      const result = { position, delay };
       if (shouldResolve) {
-        resolve({ position, delay });
+        resolve(result);
       } else {
-        reject({ position, delay });
+        reject(result);
       }
     }, delay);
-  });
+  }); 
 }
+const formEl = document.querySelector('.form');
 
-const form = document.querySelector('.form');
+formEl.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  
+  const delay = parseInt(formEl.elements.delay.value);
+  const step = parseInt(formEl.elements.step.value);
+  const amount = parseInt(formEl.elements.amount.value);
 
-function onSubmitCreatePromises(e) {
-  e.preventDefault();
-  const firstDelay = e.currentTarget.elements.delay.value;
-  const stepDelay = e.currentTarget.elements.step.value;
-  const amount = e.currentTarget.elements.amount.value;
+  for (let position = 1; position <= amount; position += 1) {
+    const currentDelay = delay + step * (position - 1);
 
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, delay).then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    }).catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-    
-    delay += step;
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
 
-  form.reset();
-}
+  
